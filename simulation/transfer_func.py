26b9e3d9c7c38ecc8d92bdbfaf04ac9fd2599a11
Dobid <davidolivares68@gmail.com>
2023-03-28 15:03:19 +0200

    airspeed openloop TF function implemented

--- simulation/transfer_func.py
+++ simulation/transfer_func.py
@@ -5,15 +5,19 @@ import control
 import numpy as np
 import matplotlib.pyplot as plt
 from models.aerodynamics import AeroModel
+from trim.trim_point import TrimPoint
 
 
-mav: AeroModel = AeroModel()
+trim: TrimPoint = TrimPoint("x8")
+uav: AeroModel = AeroModel(trim=trim)
 
-num: np.ndarray = np.array([1, 2])
-den: np.ndarray = np.array([1, 0, 4])
-H: control.TransferFunction = control.TransferFunction(num, den)
+# av1, av2, av3 TF coeffs
+av1: float = ((uav.rho * trim.Va_ms * uav.S) / uav.mass) / (uav.CDo + uav.CDalpha * trim.alpha_rad + uav.CDde * trim.elevator)
+av2: float = (uav.Pwatt / uav.Khp2w) * uav.Khp2ftlbsec
+av3: float = uav.G * np.cos(trim.theta_rad - trim.alpha_rad)
 
-p,z = control.pzmap(H)
-print('poles = ', p)
-print ('zeros = ', z)
-plt.show()
+num: list = [[ [av2], [-av3], [1] ]]
+den: list = [ [[1, av1], [1, av1], [1, av1]] ]
+H: control.TransferFunction = control.tf(num, den)
+print(H)
+pass
