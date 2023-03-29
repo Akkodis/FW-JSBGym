b4e0fd1bc2914a266c2491664408ee7ef06d2451
Dobid <davidolivares68@gmail.com>
2023-03-29 13:34:28 +0200

    updated PID, limit for throttle

--- agents/pid.py
+++ agents/pid.py
@@ -1,32 +1,41 @@
 import jsbsim
 class PID:
-    def __init__(self, kp: float = 0, ki: float = 0, kd: float = 0, dt: float = 0, limit: float = 0):
-        self.kp = kp
-        self.ki = ki
-        self.kd = kd
-        self.dt = dt
-        self.limit = limit
-        self.integral = 0
-        self.prev_error = 0
+    def __init__(self, kp: float = 0, ki: float = 0, kd: float = 0, dt: float = 0, limit: float = 0, is_throttle:bool = False):
+        self.kp: float = kp
+        self.ki: float = ki
+        self.kd: float = kd
+        self.dt: float = dt
+        self.is_throttle: bool = is_throttle
+        self.limit: float = limit
+        self.integral: float = 0.0
+        self.prev_error: float = 0.0
         self.ref: float = 0.0
 
     def set_reference(self, ref: float) -> None:
         self.ref = ref
 
     def _saturate(self, u):
-        if u >= self.limit:
-            u_sat = self.limit
-        elif u <= -self.limit:
-            u_sat = -self.limit
+        # throttle command is between 0 and 1
+        if self.is_throttle:
+            if u > self.limit:
+                u_sat = self.limit
+            if u < 0:
+                u_sat = 0
+        # flight control surfaces (aileron, elevator, rudder) are between -limit and +limit
         else:
-            u_sat = u
+            if u >= self.limit:
+                u_sat = self.limit
+            elif u <= -self.limit:
+                u_sat = -self.limit
+            else:
+                u_sat = u
         return u_sat
 
     def update(self, state: float, state_dot: float = 0, normalize: bool = True) -> float:
         error: float = self.ref - state
         self.integral += error * self.dt
         self.prev_error = error
-        output = self.kp * error + self.ki * self.integral + self.kd * state_dot
+        output: float = self.kp * error + self.ki * self.integral + self.kd * state_dot
         output = self._saturate(output)
         if normalize:
             output = self._normalize(output)
