914e11a77af3aecb7d1e3eb1b91fd68e31c07af7
Dobid <davidolivares68@gmail.com>
2023-03-31 18:12:11 +0200

    wip make oltf + pid (1)

--- simulation/oltf_pid.py
+++ simulation/oltf_pid.py
@@ -19,19 +19,18 @@ dens: list = [ [den, den, den] ]
 H: control.TransferFunction = control.tf(nums, dens)
 print(H)
 
-# t = np.linspace(0, 10, 101)
+# t = np.linspace(0, 0.5, 101)
 
-# step response
-# _, y0 = control.step_response(H, t, input=0, squeeze=True)
+# # # step response
+# # # _, y0 = control.step_response(H, t, input=0, squeeze=True)
 
-# forced response
+# # forced response
 # dt_: np.ndarray = 0.2 * np.ones(t.shape)
 # theta_: np.ndarray = np.zeros(t.shape)
 # dv: np.ndarray = np.zeros(t.shape)
 # u: np.ndarray = np.array([dt_, theta_, dv]) # commands : steps
 # _, y = control.forced_response(H, t, U=u, squeeze=True)
 # # print last output
-# print(y[-1])
 
 # # plotting
 # plt.close('all')
@@ -51,7 +50,7 @@ print(H)
 
 # plt.show()
 
-t = np.linspace(0, 20, 120)
+t: np.ndarray = np.linspace(0, 0.005, 100) # temps de simulation de 0.5s, 100 points au total
 Va_ref: float = 0.2 # set reference as deviation from Va_trim m/s
 Va_: float = 0.0 # initial value for Va_
 Va_array: list = [Va_]
@@ -62,22 +61,24 @@ errs: list = []
 K_vth: dict[str, float] = uav.compute_long_pid_gains()
 vth_pid: PID = PID(kp=K_vth['kp_vth'], ki=K_vth['ki_vth'], kd=0, dt=1/120, limit=uav.throttle_limit, is_throttle=True)
 
-tsteps: int = 200
-for i in range(0, tsteps):
+tsteps: int = 15*120
+t_pid: np.ndarray = np.linspace(0, 15, tsteps+1) # temps de simulation de 15s, 15*120 points au total (+1 à cause de la list Va_, qui n'est pas vide au début)
+for i in range(0, 15*120):
     vth_pid.set_reference(Va_ref) # je set une reference ici Va_ref = 0.2
-    cmd_th_, err = vth_pid.update(Va_, normalize=True) # update du PID, me retourne une commande et l'erreur. initialisation Va_ = 0.0
+    cmd_th_, err = vth_pid.update(Va_, normalize=False) # update du PID, me retourne une commande et l'erreur. initialisation Va_ = 0.0
     cmds_th_.append(cmd_th_) # je remplis des list pour plot
     errs.append(err)
 
     # les commandes, il y en a 3
-    dt_: np.ndarray = cmd_th_ * np.ones(t.shape) # je set une commande throttle ici cmd_th_ = 0.2
+    dt_: np.ndarray = cmd_th_ * np.ones(t.shape) # je set la commande cmd_th_ donnée par le PID, appelée dt_ dans la TF du bouquin
     theta_: np.ndarray = np.zeros(t.shape) # je set une commande theta_ nulle, vu que je veux tester uniquement le maintien de l'airspeed via une commande en throttle
     dv: np.ndarray = np.zeros(t.shape) # un terme de perturbation dv géré comme une commande dans la TF du bouquin, nul dans le cas simple
     u: np.ndarray = np.array([dt_, theta_, dv]) # la commande u est la concaténation des trois commandes dt_, theta_ et dv
 
     # je simule la réponse sur un certain temps t
-    # (on se fiche de l'axe t en soi, je ne prends que la première valeur de ttes façons)
-    _, Va_t = control.forced_response(sys=H, T=t, U=u, squeeze=True)  
+    # le temps t est de 0 à 0.5s, 100 points au total (je sais pas si c'est assez court)
+    # l'argument X0 est la condition initiale, je la set à Va_
+    _, Va_t = control.forced_response(sys=H, T=t, X0=Va_, U=u, squeeze=True)
 
     # je prends la première valeur de la réponse Va_(t), et je la stocke dans Va_
     Va_ = Va_t[-1]
@@ -85,27 +86,24 @@ for i in range(0, tsteps):
 
 
 # plotting
-t = np.linspace(0, tsteps, tsteps+1)
-print(t.shape)
-print(len(cmds_th_))
 plt.close('all')
 fig_width_cm = 24
 fig_height_cm = 18
 # plt.figure(1 , figsize =(fig_width_cm /2.54 , fig_height_cm /2.54))
 plt.subplot(3 , 1 , 1)
-plt.plot(t , Va_array ,'blue')
-plt.plot(t , Va_ref * np.ones(t.shape) ,'red')
+plt.plot(t_pid, Va_array ,'blue')
+plt.plot(t_pid , Va_ref * np.ones(t_pid.shape) ,'red')
 plt.grid()
 plt.legend(labels =('Va_', 'Va_ref'))
 
 plt.subplot(3 , 1 , 2)
-plt.plot(t[0:tsteps] , cmds_th_ ,'green')
+plt.plot(t_pid[0:tsteps] , cmds_th_ ,'green')
 # plt.xlabel('t [ s ]')
 plt.grid()
 plt.legend(labels =('cmd_th_',))
 
 plt.subplot(3 , 1 , 3)
-plt.plot(t[0:tsteps] , errs ,'black')
+plt.plot(t_pid[0:tsteps] , errs ,'black')
 plt.xlabel('t [ s ]')
 plt.grid()
 plt.legend(labels =('error',))
