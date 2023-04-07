d20c9f5dfdf0053619103cce29a90413920a7d42
Dobid <davidolivares68@gmail.com>
2023-04-07 17:04:15 +0200

    small updates

--- simulation/closed_loop/pitch.py
+++ simulation/closed_loop/pitch.py
@@ -21,19 +21,19 @@ den: list = [1, uav.a_pitch1 + (kd * uav.a_pitch3), uav.a_pitch2 + (kp * uav.a_p
 H: ctl.TransferFunction = ctl.tf(num, den)
 print(H)
 
-t = np.linspace(0, 10, 10*120)
-pitch_ref = 15.0 * (np.pi/180) * np.ones(t.shape)
+t = np.linspace(0, 10, 10*60)
+pitch_ref = 45.0 * (np.pi/180) * np.ones(t.shape)
 _, y = ctl.forced_response(H, t, U=pitch_ref)
 
 # print pitch final value
-print(f'pitch final value: {y[-1]}')
+print(f'pitch final value: {y[-1]} rad')
 
 # plotting
 plt.close('all')
-plt.title('(control) Pitch Angle PD CLFR : no convergence to ref is normal according to the book')
+plt.title('(control-FTBF) Pitch Angle PD : no convergence to ref is normal according to the book')
 plt.plot(t, y , 'blue')
 plt.plot(t, pitch_ref, 'red')
-plt.xlabel('t [s]')
+plt.ylabel('[rad]')
 plt.grid()
 plt.legend(labels =('pitch', 'pitch_ref'))
 
