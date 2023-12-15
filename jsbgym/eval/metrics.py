4262e16fd9a4886eb5be953d79076865f3371c61
Dobid <davidolivares68@gmail.com>
2023-12-15 19:49:33 +0100

    metrics computation : error bounds seem to work

--- jsbgym/eval/metrics.py
+++ jsbgym/eval/metrics.py
@@ -1,11 +1,11 @@
 import numpy as np
 from jsbgym.utils.eval_utils import State
 
-SUCCESS_THR = 100
+SUCCESS_THR = 200 # probably gotta increase this since I change refs around every 500
 
 def compute_metrics(obs, refs, steps):
     states = obs[:, :5]
-    errors = obs[:, 5:7]
+    errors = obs[:, 6:8]
     steps = steps[:2]
     steps = np.reshape(steps, (-1, steps.shape[-1]))
     for state_id, steps_per_state in zip(State, steps.T):
@@ -14,26 +14,57 @@ def compute_metrics(obs, refs, steps):
             for i, step in enumerate(steps_per_state[:-1]):
                 ref_begin = step
                 ref_end = steps_per_state[i+1]
+                print(ref_begin, ref_end)
                 error_per_ref = errors[ref_begin:ref_end, state_id]
-                print(np.rad2deg(error_per_ref))
                 success_cnt = 0
-                for j, error in enumerate(error_per_ref):
-                    if abs(error) < np.deg2rad(15): # if error is less than 5 degrees
+                for j, error in enumerate(np.flipud(error_per_ref)):
+                    if abs(error) < np.deg2rad(5): # if error is less than 5 degrees
                         success_cnt += 1
                     else: # if error is not less than 5 degrees, reset success_cnt
                         success_cnt = 0
+                        print(f"roll failed at {i}th ref, step {j}")
+                        break
                     if success_cnt >= SUCCESS_THR:
-                        print(f"roll success at {i}")
+                        print(f"roll success at {i}th ref, step {j}")
                         break
         if state_id == State.PITCH:
             print(steps_per_state)
+            for i, step in enumerate(steps_per_state[:-1]):
+                ref_begin = step
+                ref_end = steps_per_state[i+1]
+                error_per_ref = errors[ref_begin:ref_end, state_id]
+                # print(error_per_ref)
+                success_cnt = 0
+                for j, error in enumerate(np.flipud(error_per_ref)):
+                    if abs(error) < np.deg2rad(5):
+                        success_cnt += 1
+                    else:
+                        success_cnt = 0
+                        print(f"pitch failed at {i}th ref, step {j}")
+                        break
+                    if success_cnt >= SUCCESS_THR:
+                        print(f"pitch success at {i}th ref, step {j}")
+                        break
+
+# TODO: now that I have my error bounds and success I can compute the steady state error by taking the mean of the last 200 steps
+# then I can compute the rise time, settling time, and overshoot
 
 def main():
-    obs = np.load("e_obs.npy")
-    act = np.load("e_actions.npy")
+    np.set_printoptions(suppress=True)
     refs = np.load("ref_seq_arr.npy")
     steps = np.load("step_seq_arr.npy")
-    compute_metrics(obs, refs, steps)
+    
+    print("********** PPO METRICS **********")
+    ppo_obs = np.load("e_ppo_obs.npy")
+    ppo_act = np.load("e_ppo_actions.npy")
+    print(ppo_obs.shape)
+    compute_metrics(ppo_obs, refs, steps)
+
+    print("********** PID METRICS **********")
+    pid_obs = np.load("e_pid_obs.npy")
+    pid_act = np.load("e_pid_actions.npy")
+    print(pid_obs.shape)
+    compute_metrics(pid_obs, refs, steps)
 
 if __name__ == "__main__":
     main()
