0b0e814abe567bee76c286581371b510c9d932dc
Dobid <davidolivares68@gmail.com>
2024-05-21 21:40:47 +0200

    PPO train: moved save, make_env and periodic eval functions in train_utils

--- jsbgym/ppo_train.py
+++ jsbgym/ppo_train.py
@@ -10,6 +10,7 @@ from jsbgym.trim.trim_point import TrimPoint
 from jsbgym.agents import ppo
 from jsbgym.agents.pid import torchPID
 from jsbgym.utils.eval_utils import RefSequence
+from jsbgym.utils.train_utils import periodic_eval, make_env, save_model_PPO
 from jsbgym.models.aerodynamics import AeroModel
 
 import wandb
@@ -45,85 +46,6 @@ ref_seq: np.ndarray = np.array([
 												]
 											])
 
-
-def save_model(save_path, run_name, agent, env, seed):
-    print("saving agent...")
-    train_dict = {}
-    train_dict["seed"] = seed
-    train_dict["agent"] = agent.state_dict()
-    torch.save(train_dict, f"{save_path}{run_name}.pt")
-
-
-def periodic_eval(cfg_mdp, env, agent, device):
-    """Evaluate a TD-MPC2 agent."""
-    print("*** Evaluating the agent ***")
-    env.eval = True
-    ep_rewards = []
-    dif_obs = []
-    dif_fcs_fluct = [] # dicts storing all obs across all episodes and fluctuation of the flight controls for all episodes
-    for dif_idx, ref_dif in enumerate(ref_seq): # iterate over the difficulty levels
-        dif_obs.append([])
-        dif_fcs_fluct.append([])
-        for ref_idx, ref_ep in enumerate(ref_dif): # iterate over the ref for 1 episode
-            obs, info = env.reset()
-            obs, info, done, ep_reward = torch.Tensor(obs).unsqueeze(0).to(device), info, False, 0
-            while not done:
-                # Set roll and pitch references
-                env.set_target_state(ref_ep[0], ref_ep[1]) # 0: roll, 1: pitch
-                with torch.no_grad():
-                    action = agent.get_action_and_value(obs)[1].squeeze_(0).detach().cpu().numpy()
-                obs, reward, term, trunc, info = env.step(action)
-                obs = torch.Tensor(obs).unsqueeze(0).to(device)
-                done = np.logical_or(term, trunc)
-                dif_obs[dif_idx].append(info['non_norm_obs']) # append the non-normalized observation to the list
-                ep_reward += info['non_norm_reward']
-
-            ep_fcs_pos_hist = np.array(info['fcs_pos_hist'])
-            dif_fcs_fluct[dif_idx].append(np.mean(np.abs(np.diff(ep_fcs_pos_hist, axis=0)), axis=0)) # compute the fcs fluctuation of the episode being reset and append to the list
-
-            ep_rewards.append(ep_reward)
-    env.reset()
-
-    # computing the mean fcs fluctuation across all episodes for each difficulty level
-    dif_fcs_fluct = np.array(dif_fcs_fluct)
-    easy_fcs_fluct = np.mean(np.array(dif_fcs_fluct[0]), axis=0)
-    medium_fcs_fluct = np.mean(np.array(dif_fcs_fluct[1]), axis=0)
-    hard_fcs_fluct = np.mean(np.array(dif_fcs_fluct[2]), axis=0)
-
-    # computing the rmse of the roll and pitch angles across all episodes for each difficulty level
-    obs_hist_size = cfg_mdp.obs_hist_size
-    dif_obs = np.array(dif_obs)
-    if obs_hist_size == 1 and not cfg_mdp.obs_is_matrix:
-        easy_roll_rmse = np.sqrt(np.mean(np.square(dif_obs[0, :, 6])))
-        easy_pitch_rmse = np.sqrt(np.mean(np.square(dif_obs[0, :, 7])))
-        medium_roll_rmse = np.sqrt(np.mean(np.square(dif_obs[1, :, 6])))
-        medium_pitch_rmse = np.sqrt(np.mean(np.square(dif_obs[1, :, 7])))
-        hard_roll_rmse = np.sqrt(np.mean(np.square(dif_obs[2, :, 6])))
-        hard_pitch_rmse = np.sqrt(np.mean(np.square(dif_obs[2, :, 7])))
-    elif obs_hist_size > 1 and cfg_mdp.obs_is_matrix:
-        easy_roll_rmse = np.sqrt(np.mean(np.square(dif_obs[0, :, :, obs_hist_size-1, 6])))
-        easy_pitch_rmse = np.sqrt(np.mean(np.square(dif_obs[0, :, :, obs_hist_size-1, 7])))
-        medium_roll_rmse = np.sqrt(np.mean(np.square(dif_obs[1, :, :, obs_hist_size-1, 6])))
-        medium_pitch_rmse = np.sqrt(np.mean(np.square(dif_obs[1, :, :, obs_hist_size-1, 7])))
-        hard_roll_rmse = np.sqrt(np.mean(np.square(dif_obs[2, :, :, obs_hist_size-1, 6])))
-        hard_pitch_rmse = np.sqrt(np.mean(np.square(dif_obs[2, :, :, obs_hist_size-1, 7])))
-    env.eval = False
-
-    return dict(
-        episode_reward=np.nanmean(ep_rewards),
-        easy_roll_rmse=easy_roll_rmse,
-        easy_pitch_rmse=easy_pitch_rmse,
-        medium_roll_rmse=medium_roll_rmse,
-        medium_pitch_rmse=medium_pitch_rmse,
-        hard_roll_rmse=hard_roll_rmse,
-        hard_pitch_rmse=hard_pitch_rmse,
-        easy_ail_fluct=easy_fcs_fluct[0],
-        easy_ele_fluct=easy_fcs_fluct[1],
-        medium_ail_fluct=medium_fcs_fluct[0],
-        medium_ele_fluct=medium_fcs_fluct[1],
-        hard_ail_fluct=hard_fcs_fluct[0],
-        hard_ele_fluct=hard_fcs_fluct[1],
-    )
 
 
 @hydra.main(version_base=None, config_path="config", config_name="default")
@@ -162,6 +84,7 @@ def train(cfg: DictConfig):
             name=run_name,
             monitor_gym=True,
             save_code=True,
+            tags=["PPO"]
         )
         wandb.define_metric("global_step")
         wandb.define_metric("charts/*", step_metric="global_step")
@@ -186,7 +109,7 @@ def train(cfg: DictConfig):
     # env setup
     print(f"ENV ID: {cfg_ppo.env_id}")
     envs = gym.vector.SyncVectorEnv(
-        [ppo.make_env(cfg_ppo.env_id, cfg.env, cfg_sim.render_mode, None, eval=False, gamma=cfg_ppo.gamma) for i in range(cfg_ppo.num_envs)]
+        [make_env(cfg_ppo.env_id, cfg.env, cfg_sim.render_mode, None, eval=False, gamma=cfg_ppo.gamma) for i in range(cfg_ppo.num_envs)]
     )
     unwr_envs = [envs.envs[i].unwrapped for i in range(cfg_ppo.num_envs)]
     print("Single Env Observation Space Shape = ", envs.single_observation_space.shape)
@@ -245,15 +168,14 @@ def train(cfg: DictConfig):
         # save checkpoints periodically
         if cfg_ppo.save_cp and update % 8 == 0:
             run_name = f"ppo_{cfg_ppo.exp_name}_cp{global_step}_{cfg_ppo.seed}_{date_time}"
-            save_model(save_path, run_name, agent, envs.envs[0], cfg_ppo.seed)
+            save_model_PPO(save_path, run_name, agent, envs.envs[0], cfg_ppo.seed)
 
         # run periodic evaluation
         prev_div, _ = divmod(prev_gl_step, cfg_ppo.eval_freq)
         curr_div, _ = divmod(global_step, cfg_ppo.eval_freq)
         print(f"prev_gl_step = {prev_gl_step}, global_step = {global_step}, prev_div = {prev_div}, curr_div = {curr_div}")
         if cfg_ppo.periodic_eval and (prev_div != curr_div or global_step == 0):
-            eval_dict = periodic_eval(cfg_mdp, envs.envs[0], agent, device)
-            _eval_dict = dict()
+            eval_dict = periodic_eval(cfg_mdp, cfg_sim, envs.envs[0], agent, device)
             for k, v in eval_dict.items():
                 writer.add_scalar("eval/" + k, v, global_step)
 
@@ -334,7 +256,7 @@ def train(cfg: DictConfig):
                       f"episode_end={info['episode_end']}, out_of_bounds={info['out_of_bounds']}\n********")
                 writer.add_scalar("charts/episodic_return", info["episode"]["r"], global_step)
                 r_per_step = info["episode"]["r"]/info["episode"]["l"]
-                writer.add_scalar("charts/reward_per_step", r_per_step, global_step)
+                # writer.add_scalar("charts/reward_per_step", r_per_step, global_step)
                 writer.add_scalar("charts/episodic_length", info["episode"]["l"], global_step)
 
         # bootstrap value if not done
@@ -537,7 +459,7 @@ def train(cfg: DictConfig):
         pe_env = envs.envs[0]
         pe_env.eval = True
         telemetry_file = f"telemetry/{run_name}.csv"
-        pe_obs, _ = pe_env.reset(options={"render_mode": "log"})
+        pe_obs, _ = pe_env.reset(options={"render_mode": "log"} | OmegaConf.to_container(cfg_sim.eval_sim_options, resolve=True))
         pe_env.unwrapped.telemetry_setup(telemetry_file)
         pe_obs = torch.Tensor(pe_obs).unsqueeze(0).to(device)
         e_refSeq = RefSequence(num_refs=5)
@@ -569,7 +491,7 @@ def train(cfg: DictConfig):
         wandb.log({"FinalTraj/telemetry": telemetry_table})
     # Even if we don't evaluate, we still want to save the model
     else:
-        save_model(save_path, run_name, agent, envs.envs[0], cfg_ppo.seed)
+        save_model_PPO(save_path, run_name, agent, envs.envs[0], cfg_ppo.seed)
 
 
     if cfg_ppo.final_eval:
@@ -646,7 +568,7 @@ def train(cfg: DictConfig):
         print(f"Total MSE: {total_mse}")
         wandb.log({"total_mse": total_mse})
 
-    save_model(save_path, run_name, agent, envs.envs[0], cfg_ppo.seed)
+    save_model_PPO(save_path, run_name, agent, envs.envs[0], cfg_ppo.seed)
 
     envs.close()
     writer.close()
